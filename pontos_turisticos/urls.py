45658d82c3c62a968f42e1a030c2fa06f4ad4acf
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-02-23 20:35:40 -0300

    .

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -5,9 +5,11 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 
 router = routers.DefaultRouter()
 router.register('pontoturistico', PontoTuristicoViewSet)
+router.register('atracoes', AtracaoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
