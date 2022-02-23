b533290b9a30f606e747747b65ed2673d061eea9
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-02-23 20:55:49 -0300

    .

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -8,12 +8,14 @@ from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from avaliacoes.api.viewsets import AvaliacaoViewSet
 from comentarios.api.viewsets import ComentariosViewSet
+from enderecos.api.viewsets import EnderecosViewSet
 
 router = routers.DefaultRouter()
 router.register('pontoturistico', PontoTuristicoViewSet)
 router.register('atracoes', AtracaoViewSet)
 router.register('avaliacoes', AvaliacaoViewSet)
 router.register('comentarios', ComentariosViewSet)
+router.register('enderecos', EnderecosViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
