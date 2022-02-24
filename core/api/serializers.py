e61952a70fa63897256147499e7aef54f3c7c996
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2022-02-24 17:06:31 -0300

    .

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
+from enderecos.api.serializers import EnderecosSerializer
+from avaliacoes.api.serializers import AvaliacoesSerializer
+from comentarios.api.serializers import ComentariosSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    endereco = EnderecosSerializer()
+    avaliacoes = AvaliacoesSerializer(many=True)
+    comentarios = ComentariosSerializer(many=True)
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'foto')
+        fields = ('id', 'nome', 'descricao', 'foto', 'atracoes', 'avaliacoes', 'comentarios', 'endereco')
         """ fields = '__all__' """
