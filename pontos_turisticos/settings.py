abce52312e1bbac3c1e8469d6b9bfc22fcaabcd9
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2022-02-24 16:07:16 -0300

    .

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3 APP
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
     # My app
     'core',
--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -4,6 +4,7 @@ from django.db import router
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from avaliacoes.api.viewsets import AvaliacaoViewSet
@@ -20,4 +21,5 @@ router.register('enderecos', EnderecosViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token),
 ]
