e2ed4bf56758f5499dfaae9c592f74f9c8d58329
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2022-02-24 10:19:02 -0300

    .

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -10,3 +10,23 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.all()
+"""     
+    def list(self, request, *args, **kwargs):
+        return super(PontoTuristico, self).list(request, *args, **kwargs)
+    
+    def create(self, request, *args, **kwargs):
+        return super(PontoTuristico, self).create(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        return super(PontoTuristico, self).destroy(request, *args, **kwargs)
+    
+    def retrieve(self, request, *args, **kwargs):
+        return super(PontoTuristico, self).retrieve(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs):
+        return super(PontoTuristico, self).update(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        return super(PontoTuristico, self).partial_update(request, *args, **kwargs) """
+    
+    
